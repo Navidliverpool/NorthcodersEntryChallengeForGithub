9f68f8dea9167198fba1bc689cd20fe3d7e62000
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-21 00:30:09 +0100

    1.js added.

---
+++ 1.js
@@ -0,0 +1,15 @@
+/*
+Say Happy Birthday
+This function will be called with a number representing a persons age and should return a string saying 'Happy Birthday, you are today!'
+
+Examples:
+
+exclaimWord(25);
+// returns 'Happy Birthday, you are 25 today!'
+exclaimWord(7);
+// returns 'Happy Birthday, you are 7 today!'
+*/
+
+function sayHappyBirthday(number) {
+    return "Happy Birthday, you are " + number + " today!"
+}
