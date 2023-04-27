d414a8aa915d67e1a5654569290eabf177824578
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-27 17:47:53 +0100

    15MadeUp.js added

---
+++ 15MadeUp.js
@@ -0,0 +1,19 @@
+// QUESTION:  Find the characters in the array. 
+// EXAMPLE:
+// arr: [ [1, 2], ['b', 3] ]
+// RESULT:  ['b']
+function makeSingleArray(arr) {
+    let r = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (typeof(arr[i][j]) != 'number') {
+                r.push(arr[i][j])
+            }
+        }
+    }
+    return r;
+}
+console.log(makeSingleArray([
+    [1, 2],
+    ['b', 3]
+]))
